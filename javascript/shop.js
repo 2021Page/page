06d1feb295e27ae2ac7d325d91d6088732fa2f75
soyeon <athenathdus@gmail.com>
2021-06-04 17:24:24 +0900

    mouse event

---
+++ javascript/shop.js
@@ -0,0 +1,8 @@
+function OnMouseIn(n) {
+           n.style.border = "3px solid green";
+           n.style.backgroundColor = "#FAFAD2";
+       }
+ function OnMouseOut(n) {
+           n.style.border = "";
+           n.style.backgroundColor = "";
+             }
---
+++ js/shop.js
@@ -0,0 +1,8 @@
+function OnMouseIn(n) {
+           n.style.border = "1.5px solid rgb(128, 146, 199)";
+           n.style.backgroundColor = "#EBF5FF";
+       }
+ function OnMouseOut(n) {
+           n.style.border = "";
+           n.style.backgroundColor = "";
+             }
