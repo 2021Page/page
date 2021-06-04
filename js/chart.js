56bdf63bd4678e2cca9728f8d8f67f55a2f8cbc0
Hongyerim0 <dpfla927@naver.com>
2021-06-04 17:46:08 +0900

    google chart 수정

---
+++ js/chart.js
@@ -0,0 +1,16 @@
+   google.charts.load('current', {packages: ['corechart']});
+   function drawChart() {
+   // Define the chart to be drawn.
+    var data = google.visualization.arrayToDataTable([
+      ['Event Name', 'Participation Rate'],
+      ['New Member Event',  80,],
+      ['Ruminous SNS Event',  45],
+      ['Ruminous Kakao Channel Addition Event',  30]
+   ]);
+
+   var options = {title: 'Percentage of participation in events to members ( 100% )'};
+   // Instantiate and draw the chart.
+   var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+   chart.draw(data, options);
+}
+google.charts.setOnLoadCallback(drawChart);
