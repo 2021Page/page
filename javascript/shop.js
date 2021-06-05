437b0e89bb4d41912fe09e6dffffc6f6a595e7e2
Hongyerim0 <dpfla927@naver.com>
2021-06-05 22:13:52 +0900

    6.5 수정 1. shop페이지 버튼 클릭하면 이미지 변경 (자바스크립트) 2. shop detail페이지 like, cart 버튼 클릭하면 버튼 색상 변경 (자바스크립트)

--- javascript/shop.js
+++
@@ -1,8 +0,0 @@
-function OnMouseIn(n) {
-           n.style.border = "3px solid green";
-           n.style.backgroundColor = "#FAFAD2";
-       }
- function OnMouseOut(n) {
-           n.style.border = "";
-           n.style.backgroundColor = "";
-             }
--- js/shop.js
+++ js/shop.js
@@ -6,3 +6,43 @@ function OnMouseIn(n) {
            n.style.border = "";
            n.style.backgroundColor = "";
              }
+ function image_onclick(){
+   var arrImage=new Array("img/nail/nail1.png", "img/nail/nail2.png", "img/nail/nail3.png",
+     "img/nail/nail4.png", "img/nail/nail5.png","img/nail/nail6.png",
+     "img/nail/nail7.png", "img/nail/nail8.png", "img/nail/nail9.png",
+     "img/nail/nail10.png", "img/nail/nail1.png", "img/nail/nail12.png",
+     "img/nail/nail13.png", "img/nail/nail14.png", "img/nail/nail15.png",);
+       var imgSource=document.targetImg;
+       var intRandom=Math.round(Math.random()*14);
+       while(imgSource.src.indexOf(arrImage[intRandom]) != -1){
+          intRandom=Math.round(Math.random()*14);
+       }
+     imgSource.src=arrImage[intRandom];
+   }
+function image_onclick2(){
+  var arrImage=new Array("img/pedi/pedi1.png", "img/pedi/pedi2.png", "img/pedi/pedi3.png",
+    "img/pedi/pedi4.png", "img/pedi/pedi5.png","img/pedi/pedi6.png",
+    "img/pedi/pedi7.png", "img/pedi/pedi8.png", "img/pedi/pedi9.png",
+    "img/pedi/pedi10.png", "img/pedi/pedi1.png", "img/pedi/pedi12.png",
+    "img/pedi/pedi13.png", "img/pedi/pedi14.png", "img/pedi/pedi15.png",);
+   var imgSource=document.targetImg;
+   var intRandom=Math.round(Math.random()*14);
+      while(imgSource.src.indexOf(arrImage[intRandom]) != -1){
+         intRandom=Math.round(Math.random()*14);
+      }
+        imgSource.src=arrImage[intRandom];
+}
+
+function likealert(){
+  alert("The product has been reflected in the list of likes.");
+  document.getElementById("Like").style.color = "white";
+  document.getElementById("Like").style.backgroundColor = "#FF607F";
+}
+function cartalert(){
+  alert("The product is in the cart.");
+  document.getElementById("Cart").style.color = "white";
+  document.getElementById("Cart").style.backgroundColor = "#3CC8C8";
+}
+function buynowalert(){
+  alert("Your order has been completed.");
+}
