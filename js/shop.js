d1dbf06a63f7fea9c117e435742256deec45a50c
Hongyerim0 <dpfla927@naver.com>
2021-06-05 22:24:40 +0900

    detail페이지 like 버튼 누르면 이미지 테두리 색 변 (자바스크립트)

--- js/shop.js
+++ js/shop.js
@@ -36,12 +36,13 @@ function image_onclick2(){
 function likealert(){
   alert("The product has been reflected in the list of likes.");
   document.getElementById("Like").style.color = "white";
-  document.getElementById("Like").style.backgroundColor = "#FF607F";
+  document.getElementById("Like").style.backgroundColor = "#FFA2AD"
+  document.getElementById("border").style.border = "5px solid #FFA2AD";
 }
 function cartalert(){
   alert("The product is in the cart.");
   document.getElementById("Cart").style.color = "white";
-  document.getElementById("Cart").style.backgroundColor = "#3CC8C8";
+  document.getElementById("Cart").style.backgroundColor = "#61F3EB";
 }
 function buynowalert(){
   alert("Your order has been completed.");
